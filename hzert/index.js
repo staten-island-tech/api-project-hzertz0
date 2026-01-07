506dd1562d77eaa96a612fb56897a1ef50ada0bd
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2026-01-07 14:01:49 -0500

    tailwind tailwind tailwind tailwind tailwind tailwind tailwind tailwind atilwind tailwind tailwind tail wind tailwind wilatwind tailwind tailwind tailwind tailwind tailwind

--- hzert/index.js
+++ hzert/index.js
@@ -1,49 +1,20 @@
-/*async function getData() {
-  try {
-    const response = await fetch(
-      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-    );
-    const deckData = await response.json();
-    const deckId = deckData.deck_id;
-
-    const drawResponse = await fetch(
-      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
-    );
-    const drawData = await drawResponse.json();
-
-    drawData.cards.forEach((card, index) => {
-        document.querySelector(".container").insertAdjacentHTML(    
-            "afterbegin",
-            `<div class="card"">
-            <img src="${card.image}" />
-            <h2>${card.value} of ${card.suit}</h2>
-            </div>`
-  );
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getData();*/
-
 let cards = [];       // holds the deck
 let currentIndex = 0; // tracks current card
 
 async function getData() {
   try {
     const response = await fetch(
-      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1" // fetch new deck
     );
     const deckData = await response.json();
     const deckId = deckData.deck_id;
 
     const drawResponse = await fetch(
-      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52` // get whole deck
     );
     const drawData = await drawResponse.json();
 
-    cards = drawData.cards; // save cards
+    cards = drawData.cards; // save all cards in the deckid
     showCard();             // show first card
   } catch (error) {
     console.error(error);
@@ -52,15 +23,15 @@ async function getData() {
 
 function showCard() {
   if (currentIndex >= cards.length) {
-    alert("No more cards!");
+    console.log("Out of cards")
     return;
   }
 
-  const card = cards[currentIndex];
+  const card = cards[currentIndex]; // save and display current card
   const container = document.querySelector(".container");
 
   container.innerHTML = `
-    <div class="card">
+    <div class="w-100 min-h-75 text-[20px] m-3.75 rounded-2.5 p-2.5 pb-15 text-center border border-black text-black">
       <img src="${card.image}" />
       <h2>${card.value} of ${card.suit}</h2>
     </div>
